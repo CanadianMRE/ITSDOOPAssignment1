56b802be509520b30efb9699e931f15d2a9b214b
Jaymen L <jaymen100@gmail.com>
2023-06-06 17:09:01 -0600

    Added sorts

---
+++ src/project/util/Sorts.java
@@ -0,0 +1,26 @@
+package project.util;
+
+import project.shapes.Shape;
+
+public class Sorts {
+	
+	public static Shape[] BubbleSort(Shape[] shapeArray) {
+		return shapeArray;
+	}
+
+	public static Shape[] SelectionSort(Shape[] shapeArray) {
+		return shapeArray;
+	}
+
+	public static Shape[] InsertionSort(Shape[] shapeArray) {
+		return shapeArray;
+	}
+
+	public static Shape[] MergeSort(Shape[] shapeArray) {
+		return shapeArray;
+	}
+
+	public static Shape[] QuickSort(Shape[] shapeArray) {
+		return shapeArray;
+	}
+}
